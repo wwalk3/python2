1dcceb500c1bde5c8e393960322c6888fbe858b0
Will Walker <44181694+wwalk3@users.noreply.github.com>
2019-05-07 15:12:39 -0400

    Update opensecrets_final.py

--- opensecrets_final.py
+++ opensecrets_final.py
@@ -10,7 +10,8 @@ pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_colwidth', 1000)
 
-
+osAPIKEY = __OPENSECRETSAPIKEY__
+ppKEY = __PROPUBLICAAPIKEY__
 
 
 # This program has 3 main functions:
